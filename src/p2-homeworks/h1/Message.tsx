b3265a5106961182e68e29e131567ee01e3ce651
Angor78 <angor78@gmail.com>
2022-04-27 12:56:34 +0300

    Homework_1

--- src/p2-homeworks/h1/Message.tsx
+++ src/p2-homeworks/h1/Message.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import s from './Message.module.css'
 
 type MessageType = {
   avatar: string,
@@ -9,7 +10,20 @@ type MessageType = {
 
 function Message(props: MessageType) {
   return (
-    <div>
+    <div className={s.Message}>
+      <div className={s.avatar}>
+        <img src={props.avatar} alt="avatar"/>
+      </div>
+      <div className={s.content}>
+        <div className={s.content_bubble}>
+          <h4>{props.name}</h4>
+          <p>{props.message}</p>
+          <p>{props.time}</p>
+
+        </div>
+
+      </div>
+
 
     </div>
   )
