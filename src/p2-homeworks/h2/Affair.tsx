b31d68ec904cde65b8f8cdc72d3b6f59d5778735
Angor78 <angor78@gmail.com>
2022-05-05 21:51:19 +0300

    Homework_2

--- src/p2-homeworks/h2/Affair.tsx
+++ src/p2-homeworks/h2/Affair.tsx
@@ -1,21 +1,25 @@
 import React from 'react'
+import {AffairType} from "./HW2";
+import s from "./Affairs.module.css"
 
 type AffairPropsType = {
-    // key не нужно типизировать
-    affair: any // need to fix any
-    deleteAffairCallback: any // need to fix any
+  // key не нужно типизировать
+  affair: AffairType // need to fix any
+  deleteAffairCallback: (_id: number) => void // need to fix any
 }
 
 function Affair(props: AffairPropsType) {
-    const deleteCallback = () => {}// need to fix
-
-    return (
-        <div>
-            // show some text
-
-            <button onClick={deleteCallback}>X</button>
-        </div>
-    )
+  const deleteCallbackHandle = () => {
+    props.deleteAffairCallback(props.affair._id)
+  }// need to fix
+  return (
+    <div className={s.affair}>
+      <div className={s.affairName}>
+        {props.affair.name}
+      </div>
+      <button onClick={deleteCallbackHandle}>Delete</button>
+    </div>
+  )
 }
 
 export default Affair
--- src/p2-homeworks/h2/Affairs.tsx
+++ src/p2-homeworks/h2/Affairs.tsx
@@ -1,38 +1,49 @@
 import React from 'react'
 import Affair from './Affair'
-import {AffairType} from './HW2'
+import {AffairType, FilterType} from './HW2'
+import s from "./Affairs.module.css"
 
-type AffairsPropsType = { // need to fix any
-    data: any
-    setFilter: any
-    deleteAffairCallback: any
+type AffairsPropsType = {
+  data: AffairType[]
+  setFilter: (filter: FilterType) => void
+  deleteAffairCallback: (_id: number) => void
 }
 
 function Affairs(props: AffairsPropsType) {
-    const mappedAffairs = props.data.map((a: AffairType) => (
-        <Affair // should work
-            key={a._id} // кеи ОБЯЗАТЕЛЬНЫ в 99% - так что лучше их писать всегда при создании компонент в мапе
-            affair={a}
-            deleteAffairCallback={props.deleteAffairCallback}
-        />
-    ))
+  const mappedAffairs = props.data.map((a: AffairType) => (
+    <Affair // should work
+      key={a._id} // кеи ОБЯЗАТЕЛЬНЫ в 99% - так что лучше их писать всегда при создании компонент в мапе
+      affair={a}
+      deleteAffairCallback={props.deleteAffairCallback}
+    />
+  ))
 
-    const setAll = () => {} // need to fix
-    const setHigh = () => {}
-    const setMiddle = () => {}
-    const setLow = () => {}
+  const setAll = () => {
+    props.setFilter('all')
+  }
+  const setHigh = () => {
+    props.setFilter('high')
+  }
+  const setMiddle = () => {
+    props.setFilter('middle')
+  }
+  const setLowHandle = () => {
+    props.setFilter('low')
+  }
 
-    return (
-        <div>
-
-            {mappedAffairs}
-
-            <button onClick={setAll}>All</button>
-            <button onClick={setHigh}>High</button>
-            <button onClick={setMiddle}>Middle</button>
-            <button onClick={setLow}>Low</button>
-        </div>
-    )
+  return (
+    <div className={s.affairsBlock}>
+      <div>
+        <button onClick={setAll}>All</button>
+        <button onClick={setHigh}>High</button>
+        <button onClick={setMiddle}>Middle</button>
+        <button onClick={setLowHandle}>Low</button>
+      </div>
+      <div>
+        {mappedAffairs}
+      </div>
+    </div>
+  )
 }
 
 export default Affairs
