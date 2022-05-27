d1b35a012ef3ac33a4c38026578275a68d0e3726
Angor78 <angor78@gmail.com>
2022-05-27 09:56:02 +0300

    Homework_5 remove <HW1......HW4> from App and add to preJunior

--- src/p2-homeworks/h5-rrd-v6/HW5.tsx
+++ src/p2-homeworks/h5-rrd-v6/HW5.tsx
@@ -2,23 +2,25 @@ import React from 'react'
 import Header from './Header'
 import Pages from './Pages'
 import {HashRouter} from "react-router-dom";
+import PreJunior from "./pages/PreJunior";
+import Error404 from "./pages/Error404";
 
 function HW5() {
-    return (
-        <div>
-            {/*в gh-pages лучше работает HashRouter*/}
-            <HashRouter>
+  return (
+    <div>
+      {/*в gh-pages лучше работает HashRouter*/}
+      <HashRouter>
 
-            <Header/>
+        <Header/>
 
-            <Pages/>
+        <Pages/>
+        <PreJunior/>
+        <Error404/>
+      </HashRouter>
 
-            </HashRouter>
 
-
-
-        </div>
-    )
+    </div>
+  )
 }
 
 export default HW5
