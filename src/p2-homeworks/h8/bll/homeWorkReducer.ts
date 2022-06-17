e04e070450001066b0ed812de8ef854448ca9509
Angor78 <angor78@gmail.com>
2022-06-17 12:17:12 +0300

    Homework_8 done

--- src/p2-homeworks/h8/bll/homeWorkReducer.ts
+++ src/p2-homeworks/h8/bll/homeWorkReducer.ts
@@ -1,13 +1,24 @@
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
-    switch (action.type) {
-        case 'sort': {
-            // need to fix
-            return state
-        }
-        case 'check': {
-            // need to fix
-            return state
-        }
-        default: return state
+import {UserType} from "../HW8";
+
+type ActionType = {
+  type: 'sort' | 'check'
+  payload: 'up' | 'down' | number
+}
+
+export const homeWorkReducer = (state: Array<UserType>, action: ActionType): Array<UserType> => { // need to fix any
+  switch (action.type) {
+    case 'sort': {
+      let newState = [...state]
+      if (action.payload === 'up') {
+        return newState.sort((a, b) => a.age - b.age)
+      }
+      return newState.sort((a, b) => b.age - a.age)
     }
+    case 'check': {
+      let newState = [...state]
+      return newState.filter(el => el.age >= action.payload)
+    }
+    default:
+      return state
+  }
 }
--- src/p2-homeworks/h8/bll/tests/homeWorkReducer.test.tsx
+++ src/p2-homeworks/h8/bll/tests/homeWorkReducer.test.tsx
@@ -1,32 +1,34 @@
-import React from 'react'
 import {homeWorkReducer} from '../homeWorkReducer'
+import {UserType} from "../../HW8";
 
-let initialState: any[] // need to fix any
+let initialState: UserType[] // need to fix any
 
 beforeEach(() => {
-    initialState = [
-        {_id: 0, name: 'Кот', age: 3},
-        {_id: 1, name: 'Александр', age: 66},
-        {_id: 2, name: 'Коля', age: 16},
-        {_id: 3, name: 'Виктор', age: 44},
-        {_id: 4, name: 'Дмитрий', age: 40},
-        {_id: 5, name: 'Ирина', age: 55},
-    ]
+  initialState = [
+    {_id: 0, name: 'Кот', age: 3},
+    {_id: 1, name: 'Александр', age: 66},
+    {_id: 2, name: 'Коля', age: 16},
+    {_id: 3, name: 'Виктор', age: 44},
+    {_id: 4, name: 'Дмитрий', age: 40},
+    {_id: 5, name: 'Ирина', age: 55},
+  ]
 })
 
 test('sort name up', () => {
-    const newState = homeWorkReducer(initialState, {type: 'sort', payload: 'up'})
+  const newState = homeWorkReducer(initialState, {type: 'sort', payload: 'up'})
 
-    console.log(newState)
-    // expect(...).toBe(...)
+  console.log(newState)
+  expect(newState[0].name).toBe('Кот')
 })
 test('sort name down', () => {
-    const newState = homeWorkReducer(initialState, {type: 'sort', payload: 'down'})
+  const newState = homeWorkReducer(initialState, {type: 'sort', payload: 'down'})
 
-
+  expect(newState[0].name).toBe('Александр')
+  expect(newState.length).toBe(6)
 })
 test('check age 18', () => {
-    const newState = homeWorkReducer(initialState, {type: 'check', payload: 18})
+  const newState = homeWorkReducer(initialState, {type: 'check', payload: 18})
 
-
+  expect(newState[0].age >= 18).toBe(true)
+  expect(newState.length).toBe(4)
 })
