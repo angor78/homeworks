b64b4a26aa2335fda4d0c2f91a5e8129e14cdfba
Angor78 <angor78@gmail.com>
2022-07-22 12:06:40 +0300

    Homework_13 done fix1

--- src/p2-homeworks/h13/requests-api.ts
+++ src/p2-homeworks/h13/requests-api.ts
@@ -2,6 +2,6 @@ import axios from "axios";
 
 export const RequestsAPI = {
   addSuccess(success:boolean){
-    return axios.post('https:neko-cafe-back.herokuapp.com/auth/test',{success})
+    return axios.post('https://neko-cafe-back.herokuapp.com/auth/test',{success})
   }
 }
